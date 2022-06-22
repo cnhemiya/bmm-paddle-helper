c91accb2b08411d0d7b56f7404fb1927d30cbc59
cnhemiya <cnhemiya@qq.com>
2022-06-22 22:22:49 +0800

    更新 aistudio Jupyter

---
+++ tools/make_aistudio.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+TO_DIR="./run/aistudio"
+AISTUDIO_IPYNB="./examples/aistudio/main.ipynb"
+PADDLEX_CLS_IPYNB="./templates/paddlex_cls/aismain.ipynb"
+PADDLEX_DET_IPYNB="./templates/paddlex_det/aismain.ipynb"
+PADDLEX_SEG_IPYNB="./templates/paddlex_seg/aismain.ipynb"
+
+if [ ! -d $TO_DIR ]; then
+    mkdir $TO_DIR
+fi
+
+cp "$AISTUDIO_IPYNB" "$TO_DIR"
+cp "$PADDLEX_CLS_IPYNB" "$TO_DIR/paddlex_cls.ipynb"
+cp "$PADDLEX_DET_IPYNB" "$TO_DIR/paddlex_det.ipynb"
+cp "$PADDLEX_SEG_IPYNB" "$TO_DIR/paddlex_seg.ipynb"
